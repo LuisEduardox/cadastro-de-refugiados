79aca7f64891771961ef213735271cc9da3588a5
cpmcaio <caiocpmelo2@gmail.com>
2026-04-12 09:38:44 -0300

    feat: register de usuário padrão funcional

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
-# Create your views here.
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'pagina_register.html')
+    else:
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        
+        if not email or not password or not username:
+            return HttpResponse("preencher todos os campos")
+        
+        if User.objects.filter(username = username).exists():
+            return HttpResponse("username já registrado")
+                        
+        user = User.objects.create_user(username=username, email = email, password = password)
+        
+        
+        return HttpResponse(username)
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'pagina_login.html')
