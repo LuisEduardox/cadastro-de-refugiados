9ef765bfc7af82b84fc12bfca7daf3e782f4df0d
cpmcaio <caiocpmelo2@gmail.com>
2026-04-28 10:23:52 -0300

    feat: adicionando blacklist e integrando com o validador de senhas

--- accounts/validators.py
+++ accounts/validators.py
@@ -1,3 +1,5 @@
+from django.core.exceptions import ValidationError
+
 ALLOWED_EMAIL_DOMAINS = {
     'gmail.com',
     'hotmail.com',
@@ -5,7 +7,30 @@ ALLOWED_EMAIL_DOMAINS = {
     'academico.ifpb.edu.br',
 }
 
+BLACKLISTED_PASSWORDS = {
+    '123456', '123456789', 'brasil', '12345', '102030', 'senha', '12345678',
+    '1234', '10203', '123123', '123', '1234567', '654321', '1234567890',
+    'gabriel', 'abc123', 'q1w2e3r4t5y6', '101010', '159753', '123321',
+    'senha123', 'mirantte', 'flamengo', 'felicidade', 'qwerty', 'felipe',
+    '121212', '111111', '142536', 'familia', 'password', 'sucesso', 'vitoria',
+    'matheus', 'rafael', 'junior', '112233', 'gustavo', 'mariana', '1q2w3e4r',
+    '000000', 'novo', '131313', 'lucas123', 'estrela', 'daniel', 'musica',
+    'camila', 'eduardo', 'guilherme',
+}
+
 
 def email_domain_is_allowed(email):
     domain = email.split('@')[-1].lower()
     return domain in ALLOWED_EMAIL_DOMAINS
+
+
+class BlacklistedPasswordValidator:
+    def validate(self, password, user=None):
+        if password.lower() in BLACKLISTED_PASSWORDS:
+            raise ValidationError(
+                "Essa senha é muito comum. Por favor, escolha uma senha mais segura.",
+                code='password_blacklisted',
+            )
+
+    def get_help_text(self):
+        return "Sua senha não pode ser uma das senhas mais comuns."
